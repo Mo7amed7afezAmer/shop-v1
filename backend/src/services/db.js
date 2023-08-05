b3bf6a7a7ef42bccc3d8d591db4294b9c81b1ac9
Mo7amed7afezAmer <mo7amed7afez0120305@gmail.com>
2023-08-05 15:37:10 +0300

    first commit

--- backend/src/services/db.js
+++ backend/src/services/db.js
@@ -3,7 +3,7 @@ function createPool() {
       const mysql = require('mysql2');
   
       const pool = mysql.createPool({
-        host: "https://www.db4free.net",
+        host: "db4free.net",
         user: "mo7amed7afez",
         password: "mo7amed7afez_",
         database: "shop_v1",
